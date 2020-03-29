eb00ccb8c8790cc381982f1d5d9b618a06dfb570
Nazmi <nazmifeeroz@hotmail.com>
2020-03-29 21:00:59 +0800

    :recycle: refactor out stats Machine

---
+++ components/statsMachineConfig.js
@@ -0,0 +1,62 @@
+import { assign, Machine } from 'xstate'
+
+const statsApi = 'https://coronavirus-19-api.herokuapp.com/countries'
+
+const statsMachine = Machine(
+  {
+    id: 'statsMachine',
+    initial: 'fetchStats',
+    context: {
+      countriesSelected: [],
+      stats: null,
+    },
+    states: {
+      fetchStats: {
+        invoke: {
+          src: 'fetchCovidStats',
+          onDone: { target: 'ready', actions: 'assignStats' },
+          onError: 'error',
+        },
+      },
+      ready: {
+        on: {
+          COUNTRY_SELECTED: { actions: 'updateSelectedCountry' },
+          FETCH_STATS: 'fetchStats',
+        },
+      },
+      error: {},
+    },
+  },
+  {
+    actions: {
+      assignStats: assign((_ctx, e) => ({
+        stats: e.data,
+      })),
+      updateSelectedCountry: assign((ctx, e) => ({
+        countriesSelected: ctx.stats.reduce(
+          (acc, stat) =>
+            stat.country
+              .toLowerCase()
+              .match(e.country.target.value.toLowerCase())
+              ? [...acc, stat]
+              : acc,
+          [],
+        ),
+      })),
+    },
+    services: {
+      fetchCovidStats: () =>
+        new Promise(async (res, rej) => {
+          try {
+            const data = await fetch(statsApi).then(resp => resp.json())
+            return res(data)
+          } catch (error) {
+            console.log('error in fetching stats: ', error)
+            return rej()
+          }
+        }),
+    },
+  },
+)
+
+export default statsMachine
--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,14 @@
 import React from 'react'
-import { assign, Machine } from 'xstate'
+import styled from 'styled-components'
 import { useMachine } from '@xstate/react'
 import CountrySelector from '../components/CountrySelector'
 import Stat from '../components/Stat'
 import CountrySearch from '../components/CountrySearch'
 import GlobalStyles from '../components/GlobalStyles'
-
-const statsApi = 'https://coronavirus-19-api.herokuapp.com/countries'
-
-const statsMachine = Machine(
-  {
-    id: 'statsMachine',
-    initial: 'fetchStats',
-    context: {
-      countriesSelected: [],
-      stats: null,
-    },
-    states: {
-      fetchStats: {
-        invoke: {
-          src: 'fetchCovidStats',
-          onDone: { target: 'ready', actions: 'assignStats' },
-          onError: 'error',
-        },
-      },
-      ready: {
-        on: {
-          COUNTRY_SELECTED: { actions: 'updateSelectedCountry' },
-          FETCH_STATS: 'fetchStats',
-        },
-      },
-      error: {},
-    },
-  },
-  {
-    actions: {
-      assignStats: assign((_ctx, e) => ({
-        stats: e.data,
-      })),
-      updateSelectedCountry: assign((ctx, e) => ({
-        countriesSelected: ctx.stats.reduce(
-          (acc, stat) =>
-            stat.country
-              .toLowerCase()
-              .match(e.country.target.value.toLowerCase())
-              ? [...acc, stat]
-              : acc,
-          [],
-        ),
-      })),
-    },
-    services: {
-      fetchCovidStats: () =>
-        new Promise(async (res, rej) => {
-          try {
-            const data = await fetch(statsApi).then(resp => resp.json())
-            return res(data)
-          } catch (error) {
-            console.log('error in fetching stats: ', error)
-            return rej()
-          }
-        }),
-    },
-  },
-)
+import statsMachineConfig from '../components/statsMachineConfig'
 
 const HomePage = () => {
-  const [current, send] = useMachine(statsMachine)
+  const [current, send] = useMachine(statsMachineConfig)
   return (
     <div>
       <GlobalStyles />
@@ -93,8 +35,7 @@ const HomePage = () => {
 }
 
 const Footer = styled.div`
-  position: fixed;
-  bottom: 5px;
+  margin-top: 50px;
 `
 
 export default HomePage
