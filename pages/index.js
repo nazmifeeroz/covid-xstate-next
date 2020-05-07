4637c4df01fe8e7404b40732bec667b4209190e6
Nazmi <nazmifeeroz@hotmail.com>
2020-05-07 09:04:44 +0800

    :art: link to github

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useMachine } from '@xstate/react'
-import CountrySelector from '../components/CountrySelector'
-import Stat from '../components/Stat'
-import CountrySearch from '../components/CountrySearch'
-import GlobalStyles from '../components/GlobalStyles'
-import statsMachineConfig from '../components/statsMachineConfig'
+import React from "react"
+import styled from "styled-components"
+import { useMachine } from "@xstate/react"
+import CountrySelector from "../components/CountrySelector"
+import Stat from "../components/Stat"
+import CountrySearch from "../components/CountrySearch"
+import GlobalStyles from "../components/GlobalStyles"
+import statsMachineConfig from "../components/statsMachineConfig"
 
 const HomePage = () => {
   const [current, send] = useMachine(statsMachineConfig)
@@ -13,23 +13,26 @@ const HomePage = () => {
     <div>
       <GlobalStyles />
       <h3>Covid 19 information</h3>
-      {current.matches('fetchStats') && <div>loading...</div>}
-      {current.matches('error') && <div>fetching stats error</div>}
-      {current.matches('ready') && (
+      {current.matches("fetchStats") && <div>loading...</div>}
+      {current.matches("error") && <div>fetching stats error</div>}
+      {current.matches("ready") && (
         <>
           <CountrySelector
-            handleChange={country => send('COUNTRY_SELECTED', { country })}
+            handleChange={(country) => send("COUNTRY_SELECTED", { country })}
             stats={current.context.stats}
           />
           <CountrySearch
-            handleChange={country => send('COUNTRY_SELECTED', { country })}
+            handleChange={(country) => send("COUNTRY_SELECTED", { country })}
           />
         </>
       )}
       {current.context.countriesSelected.length > 0 && (
         <Stat stats={current.context.countriesSelected} />
       )}
-      <Footer>Built with &hearts; by Nazmi</Footer>
+      <Footer>
+        Built with &hearts; by{" "}
+        <a href="https://github.com/nazmifeeroz">Nazmi</a>
+      </Footer>
     </div>
   )
 }
